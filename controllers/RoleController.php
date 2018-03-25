f820a110e2bd7dea0d6d34ea747a389035cab44d
ClementIV <fyqnankai@163.com>
2018-03-25 15:03:57 +0800

    创建项目

---
+++ controllers/RoleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace clement\rest\controllers;
+
+use clement\rest\components\ItemController;
+use yii\rbac\Item;
+
+/**
+ * RoleController implements the CRUD actions for AuthItem model.
+ *
+ * @author Misbahul D Munir <misbahuldmunir@gmail.com>
+ * @since 1.0
+ */
+class RoleController extends ItemController
+{
+    /**
+     * @inheritdoc
+     */
+    public function labels()
+    {
+        return[
+            'Item' => 'Role',
+            'Items' => 'Roles',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getType()
+    {
+        return Item::TYPE_ROLE;
+    }
+}
